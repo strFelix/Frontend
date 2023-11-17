f2e58fadfb2aa2a371dbc55b02dc73bd86383338
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2023-11-16 22:29:15 -0300

    feat: creating structure for http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
@@ -13,6 +14,7 @@ import { CadastrarprodutoComponent } from './components/produtos/cadastrarprodut
 import { LOCALE_ID } from '@angular/core';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 
 registerLocaleData(localePt);
 
@@ -29,7 +31,9 @@ registerLocaleData(localePt);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' },
--- src/app/components/produtos/listarprodutos/listarprodutos.component.ts
+++ src/app/components/produtos/listarprodutos/listarprodutos.component.ts
@@ -1,3 +1,5 @@
+import { IProduto } from './../../../model/IProduto.model';
+import { ProdutosService } from './../../../services/produtos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,36 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarprodutosComponent implements OnInit {
 
-  listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
-  listaNumeros: number[] = [15, 15.18, 100];
+  listaProdutos: IProduto[] = [];
 
-  objetoModelo = {
-    nome: 'Fatima',
-    idade: 18,
-    altura: 1.56,
-    graduado: true
-  };
+  constructor(private ProdutosService: ProdutosService) {
 
-  listaProdutos: any[] = [
-    {nome: 'Curso de Angular', precoProduto: 35.56, validade: '2021-12-31', id: 1},
-    {nome: 'Curso de Ionic', precoProduto: 50, validade: '2021-12-31', id:2, promocao:true},
-    {id: 3, nome: 'Curso de Ionic Avançado', precoProduto: 50, validade: '2021-12-31'}
-  ]
-
-  constructor() {
-    for (let item of this.listaStrings) {
-      console.log(item);
-    }
-
-    for (const item of this.listaNumeros) {
-      console.log(item);
-    }
-    console.log(this.objetoModelo);
-    console.log(this.objetoModelo.nome);
-   }
-
+  }
 
   ngOnInit(): void {
   }
 
+  carregarProdutos(): void{
+    this.ProdutosService.buscarTodos().subscribe(retorno => {
+
+    })
+  }
 }
